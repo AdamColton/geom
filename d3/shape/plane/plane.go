32f58d58155236aaf128526d12ac115b2b5b115b
Adam Colton <adamcolton@gmail.com>
2019-10-22 16:53:58 +0000

    adding plane

---
+++ d3/shape/plane/plane.go
@@ -0,0 +1,59 @@
+package plane
+
+import (
+	"strings"
+
+	"github.com/adamcolton/geom/d2"
+	"github.com/adamcolton/geom/d3"
+)
+
+// Plane is defined by a point on the plane and a vector perpendicular to plan.
+type Plane struct {
+	Origin  d3.Pt
+	X, Y, N d3.V
+}
+
+// New creates a plane from 3 points. The first point is the origin on the
+// plane, the second indicates the direction that will be treated as the X axis
+// in the plane. The distance between origin and X is normalized. Ref provides a
+// third point that lies in the plane.
+func New(origin, x, ref d3.Pt) Plane {
+	dx := x.Subtract(origin).Normal()
+	n := origin.Subtract(ref).Cross(dx).Normal()
+	dy := n.Cross(dx)
+	return Plane{
+		Origin: origin,
+		X:      dx,
+		Y:      dy,
+		N:      n,
+	}
+}
+
+// Project a point onto the plane. Returns the closes point in the plane and a
+// vector from that point to the original 3d point.
+func (p Plane) Project(pt d3.Pt) (d2.Pt, d3.V) {
+	v := pt.Subtract(p.Origin)
+	vOut := p.N.Project(v)
+	vIn := v.Subtract(vOut)
+	return d2.Pt{vIn.Dot(p.X), vIn.Dot(p.Y)}, vOut
+}
+
+// Convert a 2D point in the plane to a 3D point
+func (p Plane) Convert(pt d2.Pt) d3.Pt {
+	return p.Origin.Add(p.X.Multiply(pt.X)).Add(p.Y.Multiply(pt.Y))
+}
+
+// String fulfills Stringer on Plane
+func (p Plane) String() string {
+	return strings.Join([]string{
+		"Plane( Origin:",
+		p.Origin.String(),
+		" X:",
+		p.X.String(),
+		" Y:",
+		p.Y.String(),
+		" N:",
+		p.N.String(),
+		")",
+	}, "")
+}
---
+++ d3/shape/plane/plane_test.go
@@ -0,0 +1,50 @@
+package plane
+
+import (
+	"testing"
+
+	"github.com/adamcolton/geom/d2"
+	"github.com/adamcolton/geom/d3"
+	"github.com/adamcolton/geom/geomtest"
+	"github.com/testify/assert"
+)
+
+func TestPlane(t *testing.T) {
+	p := New(d3.Pt{0, 0, 0}, d3.Pt{2, 0, 0}, d3.Pt{5, 5, 0})
+	geomtest.Equal(t, d3.Pt{0, 0, 0}, p.Origin)
+	geomtest.Equal(t, d3.V{1, 0, 0}, p.X)
+	geomtest.Equal(t, d3.V{0, 1, 0}, p.Y)
+	geomtest.Equal(t, d3.V{0, 0, 1}, p.N)
+
+	p = New(d3.Pt{0, 0, 0}, d3.Pt{2, 0, 0}, d3.Pt{5, -5, 0})
+	geomtest.Equal(t, d3.Pt{0, 0, 0}, p.Origin)
+	geomtest.Equal(t, d3.V{1, 0, 0}, p.X)
+	geomtest.Equal(t, d3.V{0, -1, 0}, p.Y)
+	geomtest.Equal(t, d3.V{0, 0, -1}, p.N)
+}
+
+func TestProject(t *testing.T) {
+	p := New(d3.Pt{0, 0, 0}, d3.Pt{2, 0, 0}, d3.Pt{5, 5, 0})
+	d2Pt, v := p.Project(d3.Pt{1, 2, 3})
+	geomtest.Equal(t, d2.Pt{1, 2}, d2Pt)
+	geomtest.Equal(t, d3.V{0, 0, 3}, v)
+
+	d2Pt, v = p.Project(d3.Pt{-1, 2, 0})
+	geomtest.Equal(t, d2.Pt{-1, 2}, d2Pt)
+	geomtest.Equal(t, d3.V{0, 0, 0}, v)
+
+	p = New(d3.Pt{0, 0, 0}, d3.Pt{2, 0, 0}, d3.Pt{5, -5, 0})
+	d2Pt, v = p.Project(d3.Pt{1, 2, 3})
+	geomtest.Equal(t, d2.Pt{1, -2}, d2Pt)
+	geomtest.Equal(t, d3.V{0, 0, 3}, v)
+	geomtest.Equal(t, d3.Pt{1, 2, 0}, p.Convert(d2Pt))
+
+	d2Pt, v = p.Project(d3.Pt{-1, 2, 0})
+	geomtest.Equal(t, d2.Pt{-1, -2}, d2Pt)
+	geomtest.Equal(t, d3.V{0, 0, 0}, v)
+}
+
+func TestString(t *testing.T) {
+	p := New(d3.Pt{0, 0, 0}, d3.Pt{2, 0, 0}, d3.Pt{5, 5, 0})
+	assert.Equal(t, "Plane( Origin:Pt(0.0000, 0.0000, 0.0000) X:V(1.0000, 0.0000, 0.0000) Y:V(0.0000, 1.0000, -0.0000) N:V(-0.0000, 0.0000, 1.0000))", p.String())
+}
