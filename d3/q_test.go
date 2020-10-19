35007d554eee1d1d6ebcd90ac3f40ddd3e4d479b
Adam Colton <adamcolton@gmail.com>
2020-10-18 20:48:15 -0400

    add Quaternion helper functions

--- d3/q_test.go
+++ d3/q_test.go
@@ -4,6 +4,8 @@ import (
 	"math"
 	"testing"
 
+	"github.com/adamcolton/geom/angle"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -76,4 +78,80 @@ func TestQ(t *testing.T) {
 
 func TestNormalizeZero(t *testing.T) {
 	assert.Equal(t, Q{}, Q{}.Normalize())
+}
+
+func TestQInv(t *testing.T) {
+	s, c := angle.Deg(30).Sincos()
+	tt := map[string]Q{
+		"ident": Q{1, 0, 0, 0},
+		"X":     Q{0, 1, 0, 0},
+		"Y":     Q{0, 0, 1, 0},
+		"Z":     Q{0, 0, 0, 1},
+		"X30":   Q{s, c, 0, 0},
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			inv, _ := tc.T().Inversion()
+			TEqual(t, inv, tc.TInv())
+		})
+	}
+}
+
+func TestQRotate(t *testing.T) {
+	tt := map[string]struct {
+		Q
+		In       Pt
+		expected Pt
+	}{
+		"Z90": {
+			Q:        QZ(angle.Deg(90)),
+			In:       Pt{1, 0, 0},
+			expected: Pt{0, 1, 0},
+		},
+		"Z45": {
+			Q:        QZ(angle.Deg(45)),
+			In:       Pt{1, 0, 0},
+			expected: Pt{0.7071, 0.7071, 0},
+		},
+		"X90": {
+			Q:        QX(angle.Deg(90)),
+			In:       Pt{0, 1, 0},
+			expected: Pt{0, 0, 1},
+		},
+		"Y90": {
+			Q:        QY(angle.Deg(90)),
+			In:       Pt{1, 0, 0},
+			expected: Pt{0, 0, -1},
+		},
+		"Z": {
+			Q:        QY(angle.Deg(90)),
+			In:       Pt{0, 0, 1},
+			expected: Pt{1, 0, 0},
+		},
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			EqualPt(t, tc.expected, tc.Q.T().Pt(tc.In))
+		})
+	}
+}
+
+func TestQV(t *testing.T) {
+	tt := map[string]V{
+		"Basic2D":            V{2, 1, 0}.Normal(),
+		"Basic2D-neg":        V{-2, 1, 0}.Normal(),
+		"Y":                  V{0, 1, 0},
+		"3D":                 V{0, 0, 1}.Normal(),
+		"3Dcomplex":          V{1, 2, 3}.Normal(),
+		"3DcomplexNonnormal": V{1, 2, 3},
+	}
+
+	for n, tc := range tt {
+		t.Run(n, func(t *testing.T) {
+			v := QV(tc).T().V(V{1, 0, 0})
+			EqualV(t, tc.Normal(), v)
+		})
+	}
 }
