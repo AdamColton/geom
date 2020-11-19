db01c818c3d48d658c33886dbd38463b3112e726
Adam Colton <adamcolton@gmail.com>
2020-11-18 22:03:05 -0500

    use children as key directly instead of hashing

--- d2/shape/boxmodel/children.go
+++ d2/shape/boxmodel/children.go
@@ -15,15 +15,3 @@ const (
 var cIter = [4]byte{0, 1, 2, 3}
 
 type children [4]uint32
-
-// bytes is use for compression, converting the children to a byte slice that
-// can be hashed.
-func (c children) bytes(out []byte) {
-	for _, child := range cIter {
-		b := c[child]
-		for i := byte(0); i < 4; i++ {
-			out[child*4+i] = byte(b)
-			b >>= 8
-		}
-	}
-}
