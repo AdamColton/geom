ca9e870c4aefbd7638e3da035a7c85dbf9ea8462
Adam Colton <adamcolton@gmail.com>
2021-09-19 19:49:36 -0400

    triangle intersector allows for faster Intersection computation

--- d3/shape/triangle/triangle.go
+++ d3/shape/triangle/triangle.go
@@ -20,34 +20,18 @@ func (t *Triangle) Intersections(l line.Line) []float64 {
 
 var Small = 1e-6
 
+func (t *Triangle) Intersector() *TriangleIntersector {
+	return &TriangleIntersector{
+		T0: t[0],
+		V1: t[1].Subtract(t[0]),
+		V2: t[2].Subtract(t[0]),
+	}
+}
+
 // Intersection returns the intersection point if there is one and bool
 // indicating if there was an intersection.
 func (t *Triangle) Intersection(l line.Line) (float64, bool) {
-	// https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-	v1 := t[1].Subtract(t[0])
-	v2 := t[2].Subtract(t[0])
-	h := l.D.Cross(v2)
-	a := v1.Dot(h)
-	if a > -epsilon && a < epsilon {
-		// line is parallel to triangle
-		return 0, false
-	}
-	f := 1.0 / a
-	s := l.T0.Subtract(t[0])
-
-	// u and v are Barycentric Coordinates
-	u := f * s.Dot(h)
-	if u < -Small || u > 1+Small {
-		// point on plane is outside triangle
-		return 0, false
-	}
-	q := s.Cross(v1)
-	v := f * l.D.Dot(q)
-	if v < -Small || u+v > 1+Small {
-		// point on plane is outside triangle
-		return 0, false
-	}
-	return f * v2.Dot(q), true
+	return t.Intersector().Intersection(l)
 }
 
 // ErrCoincidentVerticies indicates that a Triangle has at least 2 identical
@@ -70,4 +54,35 @@ func (t *Triangle) Validate() error {
 // Normal returns a vector that is perpendicular to the plane of the triangle.
 func (t *Triangle) Normal() d3.V {
 	return t[1].Subtract(t[0]).Cross(t[2].Subtract(t[0]))
+}
+
+type TriangleIntersector struct {
+	T0     d3.Pt
+	V1, V2 d3.V
+}
+
+func (t *TriangleIntersector) Intersection(l line.Line) (float64, bool) {
+	// https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+	h := l.D.Cross(t.V2)
+	a := t.V1.Dot(h)
+	if a > -epsilon && a < epsilon {
+		// line is parallel to triangle
+		return 0, false
+	}
+	f := 1.0 / a
+	s := l.T0.Subtract(t.T0)
+
+	// u and v are Barycentric Coordinates
+	u := f * s.Dot(h)
+	if u < -Small || u > 1+Small {
+		// point on plane is outside triangle
+		return 0, false
+	}
+	q := s.Cross(t.V1)
+	v := f * l.D.Dot(q)
+	if v < -Small || u+v > 1+Small {
+		// point on plane is outside triangle
+		return 0, false
+	}
+	return f * t.V2.Dot(q), true
 }
