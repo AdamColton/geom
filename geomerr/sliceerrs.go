7f25c35f42b84b69f476df972b72d8d93181d7b5
Adam Colton <adamcolton@gmail.com>
2021-12-16 19:46:42 -0500

    update SliceErr

--- geomerr/sliceerrs.go
+++ geomerr/sliceerrs.go
@@ -5,7 +5,10 @@ import (
 	"strings"
 )
 
-// SliceErrRecord represents an error comparing two slices.
+// SliceErrRecord represents an error comparing two slices. An index less than
+// 0 is treated as an error on the underlying comparison and will be printed
+// without the index. For instance, if the lengths of the slices are not the
+// same.
 type SliceErrRecord struct {
 	// Index of the reference slice
 	Index int
@@ -33,16 +36,48 @@ func (e SliceErrs) Error() string {
 
 	for i := 0; i < ln; i++ {
 		r := e[i]
-		out[i] = fmt.Sprintf("\t%d: %s", r.Index, r.Err.Error())
+		if r.Index < 0 {
+			out[i] = r.Err.Error()
+		} else {
+			out[i] = fmt.Sprintf("\t%d: %s", r.Index, r.Err.Error())
+		}
 	}
 
 	return strings.Join(out, "\n")
 }
 
-// Append a SliceErrRecord to SliceErrs using fmt.Errorf.
-func (e SliceErrs) Append(idx int, format string, args ...interface{}) SliceErrs {
+// Append a SliceErrRecord to SliceErrs. If err is nil it will not be appended.
+func (e SliceErrs) Append(idx int, err error) SliceErrs {
+	if err == nil {
+		return e
+	}
+	return append(e, SliceErrRecord{
+		Index: idx,
+		Err:   err,
+	})
+}
+
+// AppendF uses fmt.Errorf to append a SliceErrRecord to SliceErrs .
+func (e SliceErrs) AppendF(idx int, format string, args ...interface{}) SliceErrs {
 	return append(e, SliceErrRecord{
 		Index: idx,
 		Err:   fmt.Errorf(format, args...),
 	})
+}
+
+// NewSliceErrs creates an instance of SliceErrs by calling the provided func
+// for every value up to Min(lnExpected, lnActual). If lnExpected and lnActual
+// are not equal and instance of LenMismatch will be added to the start of the
+// SliceErrs with an index of -1.
+func NewSliceErrs(lnExpected, lnActual int, fn func(int) error) error {
+	var out SliceErrs
+	ln, _, lnErr := NewLenMismatch(lnExpected, lnActual)
+	out = out.Append(-1, lnErr)
+	for i := 0; i < ln; i++ {
+		out = out.Append(i, fn(i))
+	}
+	if len(out) == 0 {
+		return nil
+	}
+	return out
 }
