099eb36ff4aa25f567a92be10cfb76c136958b57
Adam Colton <adamcolton@gmail.com>
2021-12-16 19:38:59 -0500

    update ErrNotEqual

--- geomerr/notequal.go
+++ geomerr/notequal.go
@@ -9,7 +9,7 @@ type ErrNotEqual struct {
 }
 
 // NotEqual creates an instance of ErrNotEqual.
-func NotEqual(expected, actual interface{}) error {
+func NotEqual(expected, actual interface{}) ErrNotEqual {
 	return ErrNotEqual{
 		Expected: expected,
 		Actual:   actual,
@@ -19,4 +19,13 @@ func NotEqual(expected, actual interface{}) error {
 // Error fulfills the error interface.
 func (e ErrNotEqual) Error() string {
 	return fmt.Sprintf("Expected %v got %v", e.Expected, e.Actual)
+}
+
+// NewNotEqual will return nil if areEqual is true and will create an instance
+// of ErrNotEqual if it is false.
+func NewNotEqual(areEqual bool, expected, actual interface{}) error {
+	if areEqual {
+		return nil
+	}
+	return NotEqual(expected, actual)
 }
