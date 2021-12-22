6df6a8d6eb00832939223c9f891eff814a6ee032
Adam Colton <adamcolton@gmail.com>
2021-12-22 10:23:23 -0500

    Add wrapper
    GeomAssert Wraps both Equal and stretchr/testify/assert.

--- geomtest/geomtest.go
+++ geomtest/geomtest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/adamcolton/geom/calc/cmpr"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -20,6 +21,34 @@ const (
 // it will be cast to the base type.
 type AssertEqualizer interface {
 	AssertEqual(to interface{}, t cmpr.Tolerance) error
+}
+
+// GeomAssert wraps an instance of assert.Assertions but will replace calls to
+// Equal with geomtest when the type is float64 or fulfills AssertEqual. This
+// allows calls to be made without passing in testing.T each time.
+type GeomAssert struct {
+	*assert.Assertions
+	assert.TestingT
+}
+
+// New creates an instance of GeomAssert.
+func New(t assert.TestingT) *GeomAssert {
+	return &GeomAssert{
+		Assertions: assert.New(t),
+		TestingT:   t,
+	}
+}
+
+// Equal will call geomtest.EqualInDelta if expected is a float64 or if it
+// fulfills AssertEqualizer.
+func (g *GeomAssert) Equal(expected, actual interface{}, msg ...interface{}) bool {
+	if _, isAssert := expected.(AssertEqualizer); isAssert {
+		return EqualInDelta(g.TestingT, expected, actual, Small, msg...)
+	}
+	if _, isFloat := expected.(float64); isFloat {
+		return EqualInDelta(g.TestingT, expected, actual, Small, msg...)
+	}
+	return g.Assertions.Equal(expected, actual, msg...)
 }
 
 // Message takes in args to form a message. If there are more than 1 arg and the
